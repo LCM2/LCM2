96bf47813b19d76dcf919df836847d30f3ea59df
wijihoon <https://github.com/wijihoon>
2021-07-31 13:11:05 +0900

    root

---
+++ src/main/java/com/cm/lcm2/Lcm2Application.java
@@ -0,0 +1,16 @@
+package com.cm.lcm2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lcm2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lcm2Application.class, args);
+		// TEST
+		
+		
+	}
+
+}
