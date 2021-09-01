48e399b9cab1f79383fee7087c0262fad7d6a00b
fvor001 <fvor001@naver.com>
2021-09-01 16:19:10 +0900

    주석 샘플 추가

--- src/main/java/com/cm/lcm2/service/MemberService.java
+++ src/main/java/com/cm/lcm2/service/MemberService.java
@@ -14,8 +14,18 @@ public class MemberService implements IMemberService {
 	@Autowired
 	private IMailSender mailSender;
 	
+	
+	/**
+	 * Desc : 이메일 인증메일 발송
+	 * @Company : LCM
+	 * @Date    : 2021. 9. 1. 오후 4:17:26
+	 * @Author  : SuChan Kim
+	 * @param sEmail
+	 * @return
+	 * @throws Exception
+	 */
 	@Override
-	public String sendMail(String email) throws Exception {
+	public String sendMail(String sEmail) throws Exception {
 		
 		String sAutoCode = getAutoCode();
 
@@ -27,12 +37,19 @@ public class MemberService implements IMemberService {
 									        .append("</h1>")
 									        .toString());
         sendMail.setFrom(mailSender.getUsername(), "LCM");
-        sendMail.setTo(email);
+        sendMail.setTo(sEmail);
         sendMail.send();
 	    
 		return sAutoCode;
 	}
 	
+	/**
+	 * Desc : 인증번호 6자리 숫자생성
+	 * @Company : LCM
+	 * @Date    : 2021. 9. 1. 오후 4:17:20
+	 * @Author  : SuChan Kim
+	 * @return
+	 */
 	public String getAutoCode() {
 		
 		Random random=new Random();
