df54f8cd0b9eafbe5b23267e613665bc18dd29e7
wijihoon <https://wijihoon123@naver.com>
2021-09-05 21:09:13 +0900

    user 테이블 생성

--- src/main/java/com/cm/lcm2/model/Login.java
+++ src/main/java/com/cm/lcm2/model/Login.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,11 +14,24 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name="USER")
 public class Login {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private int sid;
-	private String name;
-	private double fee;
+	private int USER_NO;
+	private String USER_ID;
+	private String USER_PW;
+	private String USER_EMAIL;
+	private int USER_AGE;
+	private String USER_NICK;
+	private int USER_POSITION;
+	private int USER_CLAN;
+	private int USER_CLAN_JOIN;
+	private int USER_JOIN;
+	private int USER_RIGHT;
+	private int USER_EMAIL_FLAG;
+	private int USER_TEAM;
+	private int USER_TEAM_JOIN;
+	
 }
