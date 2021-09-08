8b1699229de7d89ced73c62547cf391238036c5f
fvor001 <fvor001@naver.com>
2021-09-08 14:08:41 +0900

    smartEditor2 멀티이미지 업로드 샘플

--- src/main/java/com/cm/lcm2/controller/SmartEditorController.java
+++ src/main/java/com/cm/lcm2/controller/SmartEditorController.java
@@ -1,6 +1,15 @@
 package com.cm.lcm2.controller;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.UUID;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,45 +17,88 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class SmartEditorController {
 
+	/**
+	 * Desc : smarteditor2 이미지 멀티업로드 (HTML5)
+	 * @Company : LCM
+	 * @Date    : 2021. 9. 8. 오후 1:23:00
+	 * @Author  : SuChan Kim
+	 * @param request
+	 * @param response
+	 */
 	@RequestMapping(value="smarteditorMultiImageUpload")
-	public String singleImageUpload(HttpServletRequest req) throws Exception {
-		
-//		String callback = smartEditor.getCallback();
-//		String callback_func = smartEditor.getCallback_func();
-//		String fileResult = "";
-//		String result = "";
-//		
-//		MultipartFile multipartFile = smartEditor.getFiledata();
-//		
-//		try {
-//			if(multipartFile != null && multipartFile.getSize() > 0 && !"".equals(multipartFile.getName()) ) {
-//				if( multipartFile.getContentType().toLowerCase().startsWith("image/") ){
-//					String originalName = multipartFile.getName();
-//					String uploadPath = req.getServletContext().getRealPath("/img");
-//					String path = uploadPath + "/smarteditor/";
-//					File file = new File(path);
-//					
-//					//경로가 없다면
-//					if(!file.exists()) {
-//						file.mkdirs();
-//					}
-//					
-//					String fileName = UUID.randomUUID().toString();
-//					smartEditor.getFiledata().transferTo(new File(path + fileName));
-//					fileResult += "&bNewLine=true&sFileName=" + originalName
-//								 + "&sFileURL=/img/smarteditor/" + fileName;
-//				}else {
-//					fileResult += "&errstr=error";
-//				}
-//			}else {
-//				fileResult += "&errstr=error";
-//			}
-//		}catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//		
-//		result = "redirect" + callback + "?callback_func=" + URLEncoder.encode(callback_func, "UTF-8") + fileResult;
-//		return result;
-		return null;
+	public void smarteditorMultiImageUpload(HttpServletRequest request, HttpServletResponse response){
+		try {
+			//파일정보
+			String sFileInfo = "";
+			//파일명을 받는다 - 일반 원본파일명
+			String sFilename = request.getHeader("file-name");
+			//파일 확장자
+			String sFilenameExt = sFilename.substring(sFilename.lastIndexOf(".")+1);
+			//확장자를소문자로 변경
+			sFilenameExt = sFilenameExt.toLowerCase();
+				
+			//이미지 검증 배열변수
+			String[] allowFileArr = {"jpg","png","bmp","gif"};
+
+			//확장자 체크
+			int nCnt = 0;
+			for(int i=0; i<allowFileArr.length; i++) {
+				if(sFilenameExt.equals(allowFileArr[i])){
+					nCnt++;
+				}
+			}
+
+			//이미지가 아니라면
+			if(nCnt == 0) {
+				PrintWriter print = response.getWriter();
+				print.print("NOTALLOW_"+sFilename);
+				print.flush();
+				print.close();
+			} else {
+				//디렉토리 설정 및 업로드	
+				
+				//파일경로
+				//application.yml로 경로 변경해야함.
+				String filePath = "src\\main\\resources\\static\\smarteditor2\\imageUpload\\";
+				File file = new File(filePath);
+				
+				if(!file.exists()) {
+					file.mkdirs();
+				}
+				
+				String sRealFileNm = "";
+				SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+				String today= formatter.format(new java.util.Date());
+				sRealFileNm = today+UUID.randomUUID().toString() + sFilename.substring(sFilename.lastIndexOf("."));
+				String rlFileNm = filePath + sRealFileNm;
+				
+				///////////////// 서버에 파일쓰기 ///////////////// 
+				InputStream inputStream = request.getInputStream();
+				OutputStream outputStream=new FileOutputStream(rlFileNm);
+				int numRead;
+				byte bytes[] = new byte[Integer.parseInt(request.getHeader("file-size"))];
+				while((numRead = inputStream.read(bytes,0,bytes.length)) != -1){
+					outputStream.write(bytes,0,numRead);
+				}
+				if(inputStream != null) {
+					inputStream.close();
+				}
+				outputStream.flush();
+				outputStream.close();
+				
+				///////////////// 이미지 /////////////////
+				// 정보 출력
+				sFileInfo += "&bNewLine=true";
+				// img 태그의 title 속성을 원본파일명으로 적용시켜주기 위함
+				sFileInfo += "&sFileName="+ sFilename;
+				sFileInfo += "&sFileURL="+"/smarteditor2/imageUpload/"+sRealFileNm;
+				PrintWriter printWriter = response.getWriter();
+				printWriter.print(sFileInfo);
+				printWriter.flush();
+				printWriter.close();
+			}	
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
