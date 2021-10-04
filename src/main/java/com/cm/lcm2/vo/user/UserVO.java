1aa2cf62f0e68e3794e4181d1cdd2d5496faed79
wijihoon <https://wijihoon123@naver.com>
2021-10-04 20:17:03 +0900

    findAll 테스트 성공

--- src/main/java/com/cm/lcm2/vo/user/UserVO.java
+++ src/main/java/com/cm/lcm2/vo/user/UserVO.java
@@ -1,5 +1,6 @@
 package com.cm.lcm2.vo.user;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,9 +9,7 @@ import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Data
 @Entity
@@ -21,19 +20,33 @@ public class UserVO {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "user_no", updatable = false, nullable = false)
 	private int userNo;
+	@Column(name = "user_id", updatable = false, nullable = false)
 	private String userId;
+	@Column(name = "user_pw", updatable = false, nullable = false)
 	private String userPw;
+	@Column(name = "user_email", updatable = false, nullable = false)
 	private String userEmail;
+	@Column(name = "user_age", updatable = false, nullable = false)
 	private int userAge;
+	@Column(name = "user_nick", updatable = false, nullable = false)
 	private String userNick;
+	@Column(name = "user_position", updatable = false, nullable = false)
 	private int userPosition;
+	@Column(name = "user_clan", updatable = false, nullable = false)
 	private int userClan;
+	@Column(name = "user_clan_join", updatable = false, nullable = false)
 	private int userClanJoin;
+	@Column(name = "user_join", updatable = false, nullable = false)
 	private int userJoin;
+	@Column(name = "user_right", updatable = false, nullable = false)
 	private int userRight;
-	private int userEmailFlag;
+	@Column(name = "user_email_flag", updatable = false, nullable = false)
+	private Integer userEmailFlag;
+	@Column(name = "user_team", updatable = false, nullable = false)
 	private int userTeam;
+	@Column(name = "user_team_join", updatable = false, nullable = false)
 	private int userTeamJoin;
 
 
